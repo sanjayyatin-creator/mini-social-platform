5d1bdfc1fb4b75f664caca9b1b347101b09273dd
sanjayyatin-creator <sanjayyatin@gmail.com>
2026-05-15 12:50:09 +0530

    Initial push

---
+++ src/main/java/com/sanjay/mini_social_platform/MiniSocialPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.sanjay.mini_social_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniSocialPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniSocialPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanjay/mini_social_platform/MiniSocialPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanjay.mini_social_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniSocialPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
